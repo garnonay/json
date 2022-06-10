0768e03240c1c01fb245e3ea9a9007e2645fe03e
garnonay <65926091+garnonay@users.noreply.github.com>
2022-06-10 10:13:07 +0200

    Add files via upload

--- json.py
+++ json.py
@@ -1,7 +1,6 @@
-
-  
 import streamlit as st
 import pandas as pd
+
 
 st.title('Conversor de R4DA - Pre')
 
@@ -14,16 +13,22 @@ slack2 = slack.replace("type", "operation")
 slack3 = slack2.replace("status: approved\n", "")
 slack4 = slack3.replace("asset: Bitcoin\n", "")
 slack5 = slack4.replace("asset: Ethereum\n", "")
-slack6 = slack5.replace("\n", ",\n")
-slack7 = slack6.replace("operation", "\"operation\"")
-slack7 = slack7.replace("netAmount", "\"netAmount\"")
-slack7 = slack7.replace("amount", "\"amount\"")
-slack7 = slack7.replace("fee", "\"fee\"")
-slack7 = slack7.replace("rate", "\"rate\"")
-slack7 = slack7.replace("id: ", "\"id\": \"")
-slack7 = slack7.replace("purchased", "\"purchased\"")
-slack7 = slack7.replace("buy", "\"buy\"")
-slack7 = slack7.replace("sell", "\"sell\"")
-if slack7 != "":
-    slack7 = "{\n" + slack7 + "\"" + "\n }"
-    st.code(slack7, language='json')
+slack6 = slack5.replace("asset: Polkadot\n", "")
+slack7 = slack6.replace("asset: Cardano\n", "")
+slack8 = slack7.replace("asset: Chainlink\n", "")
+slack9 = slack8.replace("asset: Solana\n", "")
+slack10 = slack9.replace("asset: Uniswap\n", "")
+slack11= slack10.replace("asset: Polygon\n", "")
+slack12 = slack11.replace("\n", ",\n")
+slack13 = slack12.replace("operation", "\"operation\"")
+slack13 = slack13.replace("netAmount", "\"netAmount\"")
+slack13 = slack13.replace("amount", "\"amount\"")
+slack13 = slack13.replace("fee", "\"fee\"")
+slack13 = slack13.replace("rate", "\"rate\"")
+slack13 = slack13.replace("id: ", "\"id\": \"")
+slack13 = slack13.replace("purchased", "\"purchased\"")
+slack13 = slack13.replace("buy", "\"buy\"")
+slack13 = slack13.replace("sell", "\"sell\"")
+if slack13 != "":
+    slack13 = "{\n" + slack13 + "\"" + "\n }"
+    st.code(slack13, language='json')
